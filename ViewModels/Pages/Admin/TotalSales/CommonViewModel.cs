f441b7b0e3ce7083744b602b808c45d5b9c66584
MohammadSadeghAb <113609836+MohammadSadeghAb@users.noreply.github.com>
2023-06-13 21:34:13 +0430

    ساخت ویو مدل فروش کل

---
+++ ViewModels/Pages/Admin/TotalSales/CommonViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Domain.ProductAgg;
+using Resources.Messages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels.Pages.Admin.TotalSales
+{
+    public class CommonViewModel
+    {
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.Id))]
+
+        public Guid? Id { get; set; }
+        //**********
+
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.InsertDateTime))]
+
+        public DateTime? InsertDateTime { get; set; }
+        //**********
+
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.User))]
+
+        public Guid UserId { get; set; }
+        //**********
+
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.Products))]
+
+        public List<Product> Products { get; set; }
+        //**********
+
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.Number))]
+
+        public int Number { get; set; }
+        //**********
+
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.PriceTotal))]
+
+        public int TotalPrice { get; set; }
+        //**********
+
+        //**********
+        [Required
+        (ErrorMessageResourceType = typeof(Validations),
+        ErrorMessageResourceName = nameof(Validations.Required))]
+
+        [Display
+            (ResourceType = typeof(Resources.DataDictionary),
+            Name = nameof(Resources.DataDictionary.FactorNumber))]
+
+        public string FactorNumber { get; set; }
+        //**********
+    }
+}
